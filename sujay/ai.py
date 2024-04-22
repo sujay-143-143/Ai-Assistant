f2b99aeb98e7f88600ad8e85aba78287f7421d6c
Sujay Kumar Samal <sujaysamal143143@gmail.com>
2024-04-22 20:05:03 +0530

    Update ai.py

--- sujay/ai.py
+++ sujay/ai.py
@@ -15,7 +15,7 @@ url = "https://pi.ai/talk"
 scriptDirectory = pathlib.Path().absolute()
 chrome_driver_path = 'chromedriver.exe'
 chrome_options = Options()
-chrome_options.add_argument("--headless=new")
+# chrome_options.add_argument("--headless=new")
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 chrome_options.add_argument('--log-level=3')
 service = Service(chrome_driver_path)
@@ -26,36 +26,33 @@ driver.maximize_window()
 driver.get(url)
 
 def setup():
-    button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/main/div/div/div[2]/div[2]/button')))
+    button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/main/div/div/div[2]/div[2]/div/button')))
     # Click the button
     button.click()
-
-    button1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/main/div/div[2]/div/div/div/div/div/div[1]/button[2]')))
+    button.click()
+    button.click()
+    text_area = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/div/div/div[2]/div[3]/div/div/div/div/textarea')))
+    #Enter text into the text area
+    text_area.send_keys("Sujay")
+    button1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/main/div/div/div/div[2]/div[3]/div/div/div/button')))
     # Click the button
     button1.click()
-
-    text_area = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="m_login_email"]')))
-
-    # Enter text into the text area
-    text_area.send_keys("9563902229")
-
-    text_area1 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="m_login_password"]')))
-
-    #Enter text into the text area
-    text_area1.send_keys("sujay12e")
-
-    button2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="login_password_step_element"]/button')))
+    button7 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/main/div/div/div/div[2]/div[3]/div/div/div[1]/button[4]')))
     # Click the button
-    button2.click()
-
-
-    button4 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/main/div/div/div[3]/div[2]/div[2]/div/div[2]/button')))
+    button7.click()
+    button5 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/main/div/div/div/div[2]/div[3]/div/div/div[2]/button[2]')))
     # Click the button
-    button4.click()
-
+    button5.click()
+    button6 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/main/div/div/div/div[2]/div[2]/div[2]/button')))
+    # Click the button
+    button6.click()
     text_area2 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div/div/div[3]/div[1]/div[4]/div/div/textarea')))
     #Enter text into the text area
     text_area2.send_keys("hey")
+    sleep(1)
+    button4 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/main/div/div/div[3]/div[2]/div[2]/div/div[2]/button')))
+    # Click the button
+    button4.click()
     button3 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/main/div/div/div[3]/div[1]/div[4]/div/button')))
     # Click the button
     button3.click()
